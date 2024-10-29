b1523b8be4c41dbb470406264774a2e21f5d3607
sitnikov <yellow444@gmail.com>
2024-10-29 13:18:49 +0300

    fix

--- messages.py
+++ messages.py
@@ -4,7 +4,5 @@ JCALENDAR_CALLBACK = "JCALENDAR"
 start_message = "Hey {}! I am calender bot \n \n Please type /calendar or /jcalendar to view my power"
 
 calendar_message = "Please select a date:"
-jcalendar_message = "لطفا تاریخی را انتخاب کنید:"
 
 calendar_response_message = "You selected %s"
-jcalendar_response_message = "شما تاریخ %s را انتخاب کردید"
